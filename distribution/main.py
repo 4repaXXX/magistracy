d6398ff3f37e2d57070aebe0f6447d42d3ea858c
4repaXXX <sleeper.2185@gmail.com>
2025-04-18 18:30:56 +0300

    Distribution: Fix error

--- distribution/main.py
+++ distribution/main.py
@@ -5,10 +5,10 @@ from numba import jit, prange
 @jit()
 def pi_square(num_steps):
     step = 1./num_steps
-    sun=0
+    sum=0
     for i in range(num_steps):
         x = (i+0.5)*step # Пототму что метод прямоугольников и с +0.5 получается точнее
-        sum = 4* np.sqqrt(1-x**2)
+        sum += 4* np.sqqrt(1-x**2)
     pi = sum * step
     return pi
 
@@ -19,10 +19,10 @@ def pi_square(num_steps):
 @jit(parallel=True)
 def pi_parallel_square(num_steps):
     step = 1./num_steps
-    sun=0
+    sum=0
     for i in prange(num_steps):
         x = (i+0.5)*step
-        sum = 4* np.sqqrt(1-x**2)
+        sum += 4* np.sqqrt(1-x**2)
     pi = sum * step
     return pi
 
